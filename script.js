e221af3b308fafe2657158f8b7f2d1f1dcdb7fef
Nikolay Antonovich <tesalonick@gmail.com>
2021-03-14 19:24:29 +0300

    Start1

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-class Contacts {
-
-}
-
 class Users {
     constructor(data) {
-        this.id = data.id
-        this.name = data.name
-        this.email = data.email
-        this.adress = data.adress
-        this.phone = data.phone
-
+        this.data = data
     }
     edit(obj) {
-        for (let key in obj) {
-            if (key !== undefined) {
-                this[key] = obj[key]
+        for (let key1 in obj) {
+            if (key1 !== undefined) {
+                this.data[key1] = obj[key1]
             }
-            console.log(this[key]);
-
         }
     }
+    get() {
+        console.log(this.data);
+    }
 }
-const user = new Users({
-    id: 'qq',
-    name: 'qq',
-    email: 'qq',
-    adress: 'qq',
-    phone: 'qq'
+
+class Contacts extends Users{
+    constructor(data) {
+        super(data)
+        this.dataUsers = []
+    }
+    add(data){
+        this.dataUsers.push(data)
+        // for (let key of this.dataUsers) {
+        //     console.log(key.id);
+        //     console.log(this.dataUsers);
+        // }
+        
+    }
+    edit(id, obj) {
+        super.edit(obj)
+        for (let key of this.dataUsers) {
+            if (+key.id === 2) {
+                super.edit(obj)
+                console.log(key);
+            }
+            // console.log(key.id);
+        }
+        
+
+    }
+}
+
+const list = new Contacts()
+list.add({
+    id: '1',
+    name: 'Nick',
+    email: 'Tesa@gmail.com',
+    address: 'Parokhonsk',
+    phone: '+375 44 646-44-44'
 })
-user.edit({
-    id: 'ddddd',
-    // name: 'qq',
-    email: 'ddddd',
-    adress: 'ddddd',
-    // phone: 'qq'
+list.add({
+    id: '2',
+    name: 'Valya',
+    email: 'Valya@gmail.com',
+    address: 'Lobcha',
+    phone: '+375 44 222-22-22'
 })
-console.log(user);
+list.edit(1, {
+    email: 'Nick@gmail.com',
+    address: 'Pinsk',
+})
