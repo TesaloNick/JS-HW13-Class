275850b9ef6bfac04ddf8a9c2b97329cef39bbe8
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-15 21:27:33 +0300

    add edit, remove

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 class Users {
     constructor(idData, nameData, emailData, addressData, phoneData) {
-        this.data = {id: idData, name: nameData, email: emailData, address: addressData, phone: phoneData}
+        this.data = {
+            id: idData, 
+            name: nameData, 
+            email: emailData, 
+            address: addressData, 
+            phone: phoneData
+        }
     }
     edit(obj) {
         for (let key in obj) {
@@ -17,31 +23,52 @@ class Users {
 class Contacts{
     constructor() {
         this.data = []
+        this.add = this.add.bind(this)
+        this.edit = this.edit.bind(this)
+        this.remove = this.remove.bind(this)
     }
-    add(idData, nameData, emailData, addressData, phoneData){
-        let newUser = new Users(idData, nameData, emailData, addressData, phoneData)
+    add(event){
+        event.preventDefault()
+        let newUser = new Users(event.currentTarget[0].value, event.currentTarget[1].value, event.currentTarget[2].value,  event.currentTarget[3].value, event.currentTarget[4].value)
         // console.log(newUser.data);
         this.data.push(newUser.data)
+        console.log(this.data);
     }
-    edit(id, obj) {
+    edit(event) {
+        event.preventDefault()
         for (let key of this.data) {
-            if (+key.id === 2) {
+            // console.log(key);
+            if (+key.id === +event.currentTarget[0].value) {
+                let arr = []
                 for (let key1 in key) {
-                    if (obj[key1] !== undefined){
-                        key[key1] = obj[key1]
+                    arr.push(key1)
+                }
+                console.log(arr);
+                for (let i=1; i < event.currentTarget.length-1; i++) {
+                    if (event.currentTarget[i].value !== undefined) {
+                        // console.log(key[arr[i]]);
+                        // console.log(event.currentTarget[i].value);
+                        key[arr[i]] = event.currentTarget[i].value
+                        // console.log(this.data);
                     }
                 }
+                // for (let key1 in key) {
+                    // if (obj[key1] !== undefined){
+                    //     key[key1] = obj[key1]
+                    // }
+                // }
             }
         }
-        // console.log(this.data);
+        console.log(this.data);
     }
-    remove(id) {
-        for (let i=0; i< this.data.length; i++) {
-            if (this.data[i].id === id) {
+    remove(event) {
+        event.preventDefault()
+        for (let i=0; i < this.data.length; i++) {
+            if (this.data[i].id === event.currentTarget[0].value) {
                 this.data.splice(i, 1)
             }
         }
-        // console.log(this.data);
+        console.log(this.data);
     }
     get() {
         return this.data
@@ -54,7 +81,7 @@ class ContactsApp extends Contacts{
         div.classList.add('contacts')
         document.body.appendChild(div)
 
-        let formAdd = document.createElement('form')
+        let formAdd = document.createElement('form') // добавление контакта
         formAdd.classList.add('form-add')
         div.appendChild(formAdd)
         let titleAdd = document.createElement('h1')
@@ -73,7 +100,7 @@ class ContactsApp extends Contacts{
         emailAdd.setAttribute('type', 'email')
         formAdd.appendChild(emailAdd)
         let adddressAdd = document.createElement('input')
-        adddressAdd.classList.add('adddress-add')
+        adddressAdd.classList.add('address-add')
         adddressAdd.setAttribute('type', 'text')
         formAdd.appendChild(adddressAdd)
         let phoneAdd = document.createElement('input')
@@ -90,26 +117,26 @@ class ContactsApp extends Contacts{
         buttonAddContact.innerHTML = "Добавить контакт"
         formAdd.appendChild(buttonAddContact)
 
-        let formEdit = document.createElement('form')
+        let formEdit = document.createElement('form') // изменение контакта
         formEdit.classList.add('form-edit')
         div.appendChild(formEdit)
         let titleEdit = document.createElement('h1')
         titleEdit.innerHTML = "Изменить контакт"
         formEdit.appendChild(titleEdit)
         let idEdit = document.createElement('input')
-        idEdit.classList.add('id-add')
+        idEdit.classList.add('id-edit')
         formEdit.appendChild(idEdit)
         let nameEdit = document.createElement('input')
-        nameEdit.classList.add('name-add')
+        nameEdit.classList.add('name-edit')
         formEdit.appendChild(nameEdit)
         let emailEdit = document.createElement('input')
-        emailEdit.classList.add('email-add')
+        emailEdit.classList.add('email-edit')
         formEdit.appendChild(emailEdit)
         let adddressEdit = document.createElement('input')
-        adddressEdit.classList.add('adddress-add')
+        adddressEdit.classList.add('adddress-edit')
         formEdit.appendChild(adddressEdit)
         let phoneEdit = document.createElement('input')
-        phoneEdit.classList.add('phone-add')
+        phoneEdit.classList.add('phone-edit')
         formEdit.appendChild(phoneEdit)
         idEdit.setAttribute('type', 'number')
         nameEdit.setAttribute('type', 'text')
@@ -126,7 +153,7 @@ class ContactsApp extends Contacts{
         buttonEditContact.innerHTML = "Изменить контакт"
         formEdit.appendChild(buttonEditContact)
 
-        let formRemove = document.createElement('form')
+        let formRemove = document.createElement('form') // удаление контакта
         formRemove.classList.add('form-remove')
         div.appendChild(formRemove)
         let titleRemove = document.createElement('h1')
@@ -142,27 +169,34 @@ class ContactsApp extends Contacts{
         buttonRemoveContact.innerHTML = "Удалить контакт"
         formRemove.appendChild(buttonRemoveContact)
     }
+    onAdd(){
+        document.querySelector('.form-add').addEventListener('submit', this.add)
+    }
+    onEdit(){
+        document.querySelector('.form-edit').addEventListener('submit', this.edit)
+    }
+    onRemove(){
+        document.querySelector('.form-remove').addEventListener('submit', this.remove)
+    }
 }
 
 
-const me = new Users(3, 'Nick', 'Tesa@gmail.com', 'Parokhonsk', '+375 44 646-44-44')
-me.edit({
-    email: 'Nick@gmail.com',
-    address: 'Pinsk',
-})
+const list = new ContactsApp()
+list.app()
+list.onAdd()
+list.onEdit()
+list.onRemove()
+// list.add()
+// list.add(2, 'Valya', 'Valya@gmail.com', 'Lobcha', '+375 44 222-22-22')
+// list.add(6, 'Kostya', 'Kostyaa@gmail.com', 'Luninets', '+375 44 111-11-11')
+// list.add(4, 'John', 'John@gmail.com', 'Memphis', '+375 44 222-22-22')
+// list.edit(2, {
+//     email: 'Nick@gmail.com',
+//     address: 'Pinsk',
+// })
+// list.remove(3)
 
-const list = new Contacts()
-list.add(3, 'Nick', 'Tesa@gmail.com', 'Parokhonsk', '+375 44 646-44-44')
-list.add(2, 'Valya', 'Valya@gmail.com', 'Lobcha', '+375 44 222-22-22')
-list.add(6, 'Kostya', 'Kostyaa@gmail.com', 'Luninets', '+375 44 111-11-11')
-list.add(4, 'John', 'John@gmail.com', 'Memphis', '+375 44 222-22-22')
-list.edit(2, {
-    email: 'Nick@gmail.com',
-    address: 'Pinsk',
-})
-list.remove(3)
-
-const list1 = new ContactsApp()
-list1.add(4, 'John', 'John@gmail.com', 'Memphis', '+375 44 222-22-22')
-list1.app()
-console.log(list1);
+// const list1 = new ContactsApp()
+// list1.add(4, 'John', 'John@gmail.com', 'Memphis', '+375 44 222-22-22')
+// list1.app()
+// console.log(list1);
